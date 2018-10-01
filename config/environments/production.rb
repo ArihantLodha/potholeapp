ea8e3976abbaa7dee98710d7700e9ee9ea860d10
Arihant <lodhaarihant@gmail.com>
2018-10-01 10:07:14 +0000

    Sign in Functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
+  #mail settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'potholeapp.herokuapp.com', :protocol => 'https'}
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
